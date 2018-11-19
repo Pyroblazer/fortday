6846751fb55767beffa880bd0d8cee0ca8f19e62
harryprabowo <36303295+harryprabowo@users.noreply.github.com>
2018-11-19 15:51:10 +0700

    Add files via upload

---
+++ obj.pl
@@ -0,0 +1,21 @@
+/* Weapon ID */
+weapon_id(0, hand).
+weapon_id(1, cannon).
+weapon_id(2, gun).
+
+/* Weapon Power */
+weapon_atk(hand, 5).
+weapon_atk(cannon, 100).
+weapon_atk(gun, 25).
+
+/* Armor ID */
+weapon_id(0, none).
+weapon_id(1, basic).
+weapon_id(2, deluxe).
+
+/* Armor Power */
+
+/* Medicine ID */
+med(1, a, 50).
+med(2, b, 10).
+med(3, c, 30).
